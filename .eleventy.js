1185cd1b3bd3b612b642a44db5485d5ca1dae2b1
liamfiddler <design@liamfiddler.com>
2019-06-08 12:46:03 +1000

    Add configuration options object + cache

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,64 @@
 const { JSDOM } = require('jsdom');
 const Jimp = require('jimp');
 
+const lazyImagesCache = new Map();
+
+const lazyImagesConfig = {
+  maxPlaceholderWidth: 12,
+  maxPlaceholderHeight: 12,
+  placeholderQuality: 60,
+  lazyLoadSrc: 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js',
+  imgQuery: 'img[src^="/"]',
+};
+
 const getImageData = async imageSrc => {
+  if (lazyImagesCache.has(imageSrc)) {
+    return lazyImagesCache.get(imageSrc);
+  }
+
+  console.log('LazyImages Plugin - Processing', imageSrc);
+
   const image = await Jimp.read(imageSrc);
   const width = image.bitmap.width;
   const height = image.bitmap.height;
-  const resized = image.scaleToFit(12, 12).quality(60); // FIXME: configurable size?
+
+  const resized = image
+    .scaleToFit(lazyImagesConfig.maxPlaceholderWidth, lazyImagesConfig.maxPlaceholderHeight)
+    .quality(lazyImagesConfig.placeholderQuality);
+
   const encoded = await resized.getBase64Async(Jimp.MIME_JPEG);
 
-  return {
+  const imageData = {
     width,
     height,
     src: encoded,
   };
+
+  lazyImagesCache.set(imageSrc, imageData);
+
+  return imageData;
 };
 
+// Have to use lowest common denominator JS language features here
+// because we don't know what the target browser support is
 const initLazyImages = function() {
   if ('loading' in HTMLImageElement.prototype) {
-    document.querySelectorAll('img.lazyload').forEach(function(img) {
-      img.src = img.dataset.src;
-    });
+    var images = document.querySelectorAll('img.lazyload');
+    var numImages = images.length;
+
+    if (numImages > 0) {
+      for (var i = 0; i < numImages; i++) {
+        images[i].src = images[i].dataset.src;
+      }
+    }
 
     return;
   }
 
   var script = document.createElement('script');
   script.async = true;
-  script.src =
-    'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js'; // FIXME: version?
-  document.body.appendChild(script);
+  script.src = lazyImagesConfig.lazyLoadSrc;
+  document.body.appendChild(script); // FIXME: how to use query selector here?
 };
 
 module.exports = eleventyConfig => {
@@ -37,13 +67,11 @@ module.exports = eleventyConfig => {
 
     if (outputPath.endsWith('.html')) {
       const dom = new JSDOM(content);
-      const images = dom.window.document.querySelectorAll('img'); // FIXME: probably don't want to grab ALL images
+      const images = dom.window.document.querySelectorAll(lazyImagesConfig.imgQuery);
 
       if (images.length) {
         for (let i = 0, numImages = images.length; i < numImages; i++) {
           const src = `.${images[i].src}`; // FIXME: this path is too specific
-
-          console.log('Processing', src);
 
           images[i].setAttribute('loading', 'lazy');
           images[i].setAttribute('data-src', images[i].src);
